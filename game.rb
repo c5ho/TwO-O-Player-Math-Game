c14c6d632aac8e9de952ec5ad70ceca6167f0c79
Calvin Ho <calvin.ch.ho@gmail.com>
2022-07-26 18:20:17 -0400

    Fix switching player bug and separate methods

--- game.rb
+++ game.rb
@@ -1,18 +1,28 @@
 class Game
   attr_reader :player1, :player2
 
-  #initialize 2 players, start game
+  # Initialize 2 players
   def initialize
     @player1 = Player.new("Player 1")
     @player2 = Player.new("Player 2")
+    @current_player = player1
   end
   
+  # Output the winner
+  def output_winner(winner)
+    puts "#{winner.name} wins with a score of #{winner.lives}/#{Player.max_lives}!"
+    puts "----------GAME OVER----------"
+    exit(0)
+  end
+
   # Get new question
-  def next_turn
+  def get_question
     print "#{@current_player.name}: "
     @question = Question.new
-    
-    # Prompt for and get response
+  end
+
+  # Get response and check if it's correct
+  def get_response
     print ">"
     @response = $stdin.gets.chomp.to_i
     if @question.is_correct(@response.to_i)
@@ -21,28 +31,37 @@ class Game
       puts "#{@current_player.name}: Seriously?  No!"
       @current_player.decrement
     end
-    puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3\n\n"
-        
-    if player1.lives == 0
-      puts "#{player2.name} wins with a score of #{player2.lives}/3!"
-      puts "----------GAME OVER----------"
-      exit(0)
-    elsif player2.lives ==0
-      puts "#{player1.name} wins with a score of #{player1.lives}/3!"
-      puts "----------GAME OVER----------"
-      exit(0)
-    end
+  end
+  
+  def output_stats
+    puts "P1: #{player1.lives}/#{Player.max_lives} vs P2: #{player2.lives}/#{Player.max_lives}\n\n"
+  end
+
+  def prep_next_turn
     puts "----------NEW TURN----------"
-    if @current_player = player1 
+    if @current_player == player1 
       @current_player = player2
-    elsif @current_player = player2
+    elsif @current_player == player2
       @current_player = player1
     end
+  end
+
+  def next_turn
+    get_question
+    get_response    
+    output_stats
+
+    if player1.lives == 0
+      output_winner(player2)
+    elsif player2.lives ==0
+      output_winner(player1)
+    end
+    
+    prep_next_turn
     next_turn
   end
   
    def start
-    @current_player = player1
     next_turn
   end
 end
--- player.rb
+++ player.rb
@@ -1,11 +1,17 @@
 class Player 
   attr_accessor :lives, :name
+  @@max_lives = 3
 
   def initialize(name)
     @name = name
     @lives = 3;
     puts "Player #{name} initialized with #{lives} lives."
   end
+
+  def self.max_lives
+    @@max_lives  
+  end
+
 
   def decrement
     @lives -= 1
